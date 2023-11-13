37ee60cf339c72f7e0368639bc1fcd5f2d219cc1
Ethan <ethan.scott2@icloud.com>
2023-11-13 09:34:55 -0600

    Initial Commit

---
+++ Three letter word/Three_letter_wordApp.swift
@@ -0,0 +1,17 @@
+//
+//  Three_letter_wordApp.swift
+//  Three letter word
+//
+//  Created by Ethan Scott on 11/13/23.
+//
+
+import SwiftUI
+
+@main
+struct Three_letter_wordApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
